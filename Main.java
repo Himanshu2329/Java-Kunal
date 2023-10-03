3253e0b0a27165df7cef4f031c129fa11b8afb98
Himanshu2329 <kumarhimanshu4390@gmail.com>
2023-10-04 00:49:50 +0530

    Hello World

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World");
+        
+    }
+}
